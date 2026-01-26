d8e94eff4d102e2689a1891a0b29c57b4f4e0f65
Debashish mallick <mdebashish543@gmail.com>
2026-01-26 12:38:55 +0530

    Start Alisi-Technology

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,29 +1,24 @@
-import { useState } from 'react'
-import {Route, Routes} from 'react-router-dom'
-import './App.css'
-import Navbar from './Pages/Navbar/Navbar'
-import Footer from './Pages/Footer/Footer'
-import Home from './Pages/Home/Home'
-import About from './Pages/About/About'
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Pages/Navbar/Navbar";
+import Footer from "./Pages/Footer/Footer";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
 
 function App() {
- 
-
   return (
     <>
-    <Navbar/>
+      <Navbar />
 
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/about' element={<About/>} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
 
-    </Routes>
-
-
-    <Footer/>
-    
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
