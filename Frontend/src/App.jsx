39bd5804db3cd5f4b44f266a9717e9e692716080
Debashish mallick <mdebashish543@gmail.com>
2026-01-26 12:29:52 +0530

    Start Alisi-Technology

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
+import {Route, Routes} from 'react-router-dom'
 import './App.css'
+import Navbar from './Pages/Navbar/Navbar'
+import Footer from './Pages/Footer/Footer'
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-     
+    <Navbar/>
+
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+
+    </Routes>
+
+
+    <Footer/>
+    
     </>
   )
 }
