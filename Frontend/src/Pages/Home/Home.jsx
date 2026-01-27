078b53112bf4ece9c489dfcfab0ce67b0cc55ea1
swarnaprava-06032005 <150228737+swarnaprava-06032005@users.noreply.github.com>
2026-01-27 11:00:17 +0530

    push changes

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import "./Home.css"
+import HomeHeroSection from '../../Components/HomeHeroSection/HomeHeroSection'
+import HomeTestimonial from '../../Components/HomeTestimonial/HomeTestimonial'
+import HomeSliderBrand from '../../Components/HomeSliderBrand/HomeSliderBrand'
+import HomeWhyChoose from '../../Components/HomeWhyChoose/HomeWhyChoose'
+
 
 const Home = () => {
   return (
     <div className='home'>
-      Home
+      <HomeHeroSection/>
+      <HomeSliderBrand/>
+      <HomeWhyChoose/>
+      <HomeTestimonial/>
     </div>
   )
 }
