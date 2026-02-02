5c2b15f50cae87e313ac3f95a08f676a033c4d29
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2026-02-02 10:38:30 +0530

    For Pull

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,13 +6,10 @@ import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import Contact from "./Pages/Contact/Contact";
 import AllServices from "./Pages/AllServices/AllServices";
-<<<<<<< HEAD
 import AiDataAnalytics from "./Pages/AiDataAnalytics/AiDataAnalytics"
 import Cloud from "./Pages/Cloud/Cloud";
 import Consulting from "./Pages/Consulting/Consulting";
-=======
 import Projects from "./Pages/Projects/Projects";
->>>>>>> 5764ff8e3bcbf31752edd5b36540656ecb0c661e
 
 function App() {
   return (
@@ -24,13 +21,10 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/services/all-services" element={<AllServices />} />
-<<<<<<< HEAD
         <Route path="/services/Data-Analytics"element={<AiDataAnalytics />} />
        <Route path="/services/Cloud" element={<Cloud />} />
        <Route path="/services/Consulting" element={<Consulting />} />
-=======
         <Route path="/projects" element={<Projects />} />
->>>>>>> 5764ff8e3bcbf31752edd5b36540656ecb0c661e
       </Routes>
 
       <Footer />
